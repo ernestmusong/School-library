4a9460b07c8bb66a16587f5f53ff818b021b5d70
Musong <ernestakeh@gmail.com>
2023-05-16 06:11:22 +0100

    Fix readme

--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,12 @@
 class Teacher < Person
-    attr_reader :specialization
-  
-    def initialize(name: 'Unknown', age: 0, parent_permission: true, specialization: '')
-      super(name: name, age: age, parent_permission: parent_permission)
-      @specialization = specialization
-    end
-  
-    def can_use_services?
-      true
-    end
+  attr_reader :specialization
+
+  def initialize(name: 'Unknown', age: 0, parent_permission: true, specialization: '')
+    super(name: name, age: age, parent_permission: parent_permission)
+    @specialization = specialization
   end
+
+  def can_use_services?
+    true
+  end
+end

f915ce1e5d5a8753e5ad225c422c910ef2939522
Musong <ernestakeh@gmail.com>
2023-05-18 05:35:33 +0100

    Add classroom, book, and rental classes

--- classes/person.rb
+++ classes/person.rb
@@ -5,7 +5,7 @@ require_relative '../decorators/trimer_decorator'
 
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(name: 'Unknown', age: 0, parent_permission: true)
     super()
@@ -13,6 +13,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def of_age?
@@ -29,7 +30,7 @@ class Person < Nameable
   end
 end
 
-person = Person.new(name: 'maximilianus', age: 22)
+person = Person.new(name: 'maximttttttttt', age: 22)
 puts person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name

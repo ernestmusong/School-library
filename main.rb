9708831e2ded6fe6ffb351d7ec3a7c7741885aca
codedit334 <oussama7elabdioui@gmail.com>
2023-05-22 13:53:45 +0100

    Create UI module

--- main.rb
+++ main.rb
@@ -1,10 +1,12 @@
 require_relative './classes/app'
+require_relative './modules/UIMethods'
 require_relative './modules/options'
 require_relative './modules/presentations'
 
 class Main
   include Options
   include Presentations
+  include UIMethods
   
   def initialize
     @app = App.new
@@ -15,18 +17,8 @@ class Main
   def run
     loop do
       options
-      input = gets.chomp
-      case input.to_i
-      when 1 then @app.list_books
-      when 2 then @app.list_people
-      when 3 then @app.create_person
-      when 4 then @app.create_book
-      when 5 then @app.create_rental
-      when 6 then @app.list_rentals
-      when 7
-        puts 'Thank you for using School Library App!'
-        return
-      end
+      get_options
+
     end
   end
 end
---
+++ modules/UIMethods.rb
@@ -0,0 +1,17 @@
+module UI
+
+    def get_options
+      input = gets.chomp
+      case input.to_i
+      when 1 then @app.list_books
+      when 2 then @app.list_people
+      when 3 then @app.create_person
+      when 4 then @app.create_book
+      when 5 then @app.create_rental
+      when 6 then @app.list_rentals
+      when 7
+        puts 'Thank you for using School Library App!'
+        return
+      end
+    end
+end

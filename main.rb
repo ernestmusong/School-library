5dbafd510f9b8e4eaa311f558bba6aa10da521f5
Musong <ernestakeh@gmail.com>
2023-05-22 13:32:04 +0100

    create modules folder and options file

--- main.rb
+++ main.rb
@@ -1,20 +1,13 @@
 require_relative './classes/app'
+require_relative './modules/options'
 
 class Main
+include Options
   def initialize
     @app = App.new
   end
   puts "Welcome to School Library\n\n"
   puts "Please choose an option by entering a number!\n"
-  def options
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
-  end
 
   def run
     loop do

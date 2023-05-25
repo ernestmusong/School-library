755c4a9d220b7817aa9269b7156dd20f7bda8ea0
Matthieu96Code <senoumatthhieu96@gmail.com>
2023-05-25 08:19:30 +0000

    Add classroom tests

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,27 @@
+require './classes/classroom'
+
+RSpec.describe Classroom do
+  let(:classroom_label) { "Mathematics" }
+  let(:classroom) { Classroom.new(classroom_label) }
+  let(:student) { double("student") }
+
+  describe "#initialize" do
+    it "creates a new Classroom instance with a label" do
+      expect(classroom.label).to eq(classroom_label)
+    end
+
+    it "initializes students as an empty array" do
+      expect(classroom.students).to be_empty
+    end
+  end
+
+  describe "#add_student" do
+    context "when student is not already in the classroom" do
+
+      it "sets the classroom for the student" do
+        expect(student).to receive(:classroom=).with(classroom)
+        classroom.add_student(student)
+      end
+    end
+  end
+end
